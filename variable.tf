02d287bed19c5e2d5dd89d968579ab264c6b896f
Tejendra Sapkota <sapkota30@gmail.com>
2025-12-03 20:43:54 -0500

    serverless app function

---
+++ variable.tf
@@ -0,0 +1,18 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS region to deploy into"
+  default     = "ca-central-1"
+}
+
+variable "bucket_name" {
+  type        = string
+  description = "Enter Your Project Bucket Name"
+  default     = "metroc-ccp-aws-project2-app2-tejendra"
+}
+
+variable "glue_bucket_name" {
+  type        = string
+  description = "Enter Your Glue Bucket Name"
+  default     = "metroc-ccp-aws-project2-app3-glue-tejendra"
+}
+}
